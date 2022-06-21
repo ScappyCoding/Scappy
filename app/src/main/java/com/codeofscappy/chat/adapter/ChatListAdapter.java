9c14f7474e15f56172d09c55f0230dac858757e6
ScappyCoding <MaurinHerrmann@outlook.com>
2022-06-21 11:08:24 +0200

    Implement: package: [adapter], [menu], [model] ChatListAdapter, Chatlist implements Test-User in Chat-list-Fragment

---
+++ app/src/main/java/com/codeofscappy/chat/adapter/ChatListAdapter.java
@@ -0,0 +1,71 @@
+package com.codeofscappy.chat.adapter;
+
+
+import static com.bumptech.glide.Glide.*;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codeofscappy.chat.R;
+import com.codeofscappy.chat.model.Chatlist;
+import com.mikhaellopez.circularimageview.CircularImageView;
+
+import java.util.List;
+
+public class ChatListAdapter extends RecyclerView.Adapter<ChatListAdapter.Holder> {
+    private List<Chatlist> list;
+    private Context context;
+
+    public ChatListAdapter(List<Chatlist> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.layout_chat_list,parent, false);
+    return new Holder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Holder holder, int position) {
+
+        Chatlist chatlist = list.get(position);
+
+        holder.tvName.setText(chatlist.getUserName());
+        holder.tvDesc.setText(chatlist.getDescription());
+        holder.tvDate.setText(chatlist.getDate());
+
+        // for image i need API-library....
+        Glide.with(context).load(chatlist.getUrlProfile()).into(holder.profile);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+        private TextView tvName, tvDesc, tvDate;
+        private CircularImageView profile;
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+
+            tvDate = itemView.findViewById(R.id.tv_Date);
+            tvDesc = itemView.findViewById(R.id.tv_Desc);
+            tvName = itemView.findViewById(R.id.tv_Name);
+            profile = itemView.findViewById(R.id.image_profile);
+        }
+    }
+}
--- app/src/main/java/com/codeofscappy/chat/menu/ChatsFragment.java
+++ app/src/main/java/com/codeofscappy/chat/menu/ChatsFragment.java
@@ -3,15 +3,22 @@ package com.codeofscappy.chat.menu;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codeofscappy.chat.R;
+import com.codeofscappy.chat.adapter.ChatListAdapter;
+import com.codeofscappy.chat.model.Chatlist;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
- * A simple {@link Fragment} subclass.
+ * Asimple {@link Fragment} subclass.
  */
 public class ChatsFragment extends Fragment {
 
@@ -21,11 +28,28 @@ public class ChatsFragment extends Fragment {
         // Required empty public constructor
     }
 
+    private List<Chatlist> list = new ArrayList<>();
+    private RecyclerView recyclerView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chats, container, false);
+        View view = inflater.inflate(R.layout.fragment_chats, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        getChatlist();
+        return  view;
     }
+
+    // UserList [-Demo_User-]--> Real User Implement later!
+    private void getChatlist() {
+        list.add(new Chatlist("11","Demo_User","Demo_Status","21.06.2022","https://pickaface.net/gallery/avatar/20160608_151321_4867_Jeffyou.png"));
+        list.add(new Chatlist("12","Demo_User2","Demo_Status","21.06.2022","https://pickaface.net/gallery/avatar/20160110_040511_326_demo.png"));
+
+        recyclerView.setAdapter(new ChatListAdapter(list,getContext()));
+    }
+
 }
