60cd98e11272ee37fe35778321902e478a253c32
ScappyCoding <MaurinHerrmann@outlook.com>
2022-06-27 18:41:29 +0200

    Implement: BottomSheetDialog
    Gallery_Pick Image from Gallery into --> CircularImageView
    
    Camera-Pick ShotImage from Camera into -->CircularImageView

--- app/src/main/java/com/codeofscappy/chat/view/profile/ProfileActivity.java
+++ app/src/main/java/com/codeofscappy/chat/view/profile/ProfileActivity.java
@@ -3,12 +3,17 @@ package com.codeofscappy.chat.view.profile;
 import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
@@ -23,6 +28,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.mikhaellopez.circularimageview.CircularImageView;
 
 import java.util.Objects;
 
@@ -33,6 +39,10 @@ public class ProfileActivity extends AppCompatActivity {
     private FirebaseFirestore firestore;
 
     private BottomSheetDialog bottomSheetDialog;
+
+    private int IMAGE_GALLERY_REQUEST = 111;
+    private int CAMERA_PIC_REQUEST = 112;
+    private Uri imageUri, cameraUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -81,6 +91,26 @@ public class ProfileActivity extends AppCompatActivity {
     private void showBottomSheetPickPhoto() {
           View view = getLayoutInflater().inflate(R.layout.bottom_sheet_pick,null);
 
+        ((View) view.findViewById(R.id.ln_gallery)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+                bottomSheetDialog.dismiss();
+
+            }
+        });
+
+        // test Camera
+        ((View) view.findViewById(R.id.ln_camera)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCamera();
+                Toast.makeText(ProfileActivity.this, "Camera Mode, Not possible yet", Toast.LENGTH_SHORT).show();
+                bottomSheetDialog.dismiss();
+
+            }
+        });
+
         bottomSheetDialog = new BottomSheetDialog(this);
         bottomSheetDialog.setContentView(view);
 
@@ -103,7 +133,6 @@ public class ProfileActivity extends AppCompatActivity {
 
 
 
-
     // This Methode Read the Data from Current-User and Push this Into the View-Elements
     private void getInfo() {
         firestore.collection("Users").document(firebaseUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -122,4 +151,55 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void openGallery() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"select image"), IMAGE_GALLERY_REQUEST);
+    }
+    // Testing Camera
+    private void openCamera() {
+        Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraintent, CAMERA_PIC_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == IMAGE_GALLERY_REQUEST
+        && resultCode == RESULT_OK
+        && data != null
+        && data.getData() != null) {
+
+            imageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+                binding.imageProfile.setImageBitmap(bitmap);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+       // Testing Camera Pick Image
+      if (requestCode == CAMERA_PIC_REQUEST){
+          Bundle extras = data.getExtras();
+          Bitmap image = (Bitmap) data.getExtras().get("data");
+          CircularImageView circularImageView = (CircularImageView) findViewById(R.id.image_profile); // Set image as Bitmap
+          circularImageView.setImageBitmap(image);
+      }
+
+
+
+    }
+
+
+
+
+
+
+
+
 }
