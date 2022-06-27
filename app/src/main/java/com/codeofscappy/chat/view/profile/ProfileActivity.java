4a4d02f1a51d4e847f9c439e400e2d448fcea0f9
ScappyCoding <MaurinHerrmann@outlook.com>
2022-06-27 16:13:06 +0200

    Implement: BottomSheetDialog
    BottomSheetPick: --> Design-Layout
    BottomSheetDialog: --> Gallery, Camera

--- app/src/main/java/com/codeofscappy/chat/view/profile/ProfileActivity.java
+++ app/src/main/java/com/codeofscappy/chat/view/profile/ProfileActivity.java
@@ -1,9 +1,14 @@
 package com.codeofscappy.chat.view.profile;
 
+import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
@@ -11,10 +16,23 @@ import androidx.databinding.DataBindingUtil;
 import com.codeofscappy.chat.R;
 import com.codeofscappy.chat.databinding.ActivityProfileBinding;
 import com.codeofscappy.chat.view.settings.SettingsActivity;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private ActivityProfileBinding binding;
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore firestore;
+
+    private BottomSheetDialog bottomSheetDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +40,7 @@ public class ProfileActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_profile);
 
 
+        // Implement Toolbar BackArrow get BackOption
         Toolbar toolbar = (Toolbar)  findViewById(R.id.toolbar);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_24);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -32,12 +51,75 @@ public class ProfileActivity extends AppCompatActivity {
         });
 
 
+        // Instance of Firebase Database
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        firestore = FirebaseFirestore.getInstance();
+
+        if (firebaseUser != null){
+            getInfo();
+        }
+        
+        initActionClick();
+
+
+
+
+    }
+    // Button for BottomSheetDialog-Layout
+    private void initActionClick() {
+        binding.fabCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showBottomSheetPickPhoto();
+            }
+        });
+    }
+
+    // Methode for the DialogSheet
+
+    @SuppressLint("ObsoleteSdkInt")
+    private void showBottomSheetPickPhoto() {
+          View view = getLayoutInflater().inflate(R.layout.bottom_sheet_pick,null);
+
+        bottomSheetDialog = new BottomSheetDialog(this);
+        bottomSheetDialog.setContentView(view);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Objects.requireNonNull(bottomSheetDialog.getWindow()).addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+        bottomSheetDialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                bottomSheetDialog = null;
+            }
+        });
+
+        bottomSheetDialog.show();
+
+
+    }
 
 
 
 
 
+    // This Methode Read the Data from Current-User and Push this Into the View-Elements
+    private void getInfo() {
+        firestore.collection("Users").document(firebaseUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                String userName = documentSnapshot.get("userName").toString();
+                String userPhone = documentSnapshot.get("userPhone").toString();
+                binding.tvUsername.setText(userName);
+                binding.tvPhone.setText(userPhone);
 
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
 
+            }
+        });
     }
 }
