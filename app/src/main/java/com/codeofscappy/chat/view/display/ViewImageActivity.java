fd495fcc0c6e04952e1b062a3cb2340fcc9d3d31
ScappyCoding <MaurinHerrmann@outlook.com>
2022-06-29 15:39:19 +0200

    Update: ViewImageActivity Button --> BackArrow to (ProfileActivity)

--- app/src/main/java/com/codeofscappy/chat/view/display/ViewImageActivity.java
+++ app/src/main/java/com/codeofscappy/chat/view/display/ViewImageActivity.java
@@ -1,6 +1,8 @@
 package com.codeofscappy.chat.view.display;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -8,6 +10,7 @@ import androidx.databinding.DataBindingUtil;
 import com.codeofscappy.chat.R;
 import com.codeofscappy.chat.common.Common;
 import com.codeofscappy.chat.databinding.ActivityViewImageBinding;
+import com.codeofscappy.chat.view.profile.ProfileActivity;
 
 public class ViewImageActivity extends AppCompatActivity {
 
@@ -19,6 +22,14 @@ public class ViewImageActivity extends AppCompatActivity {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_view_image);
 
         binding.ImageView.setImageBitmap(Common.IMAGE_BITMAP);
+
+        //Toolbar Navigation: --> Button --> Back-Arrow
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ViewImageActivity.this, ProfileActivity.class));
+            }
+        });
 
 
     }
